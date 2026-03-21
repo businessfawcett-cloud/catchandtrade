85a2cbca1395f0efa3a209e0e634114e6402ed73
businessfawcett-cloud <fawcettbusiness@gmail.com>
2026-03-21 13:43:37 -0600

    Fixed workspaces to exclude mobile app

--- apps/web/vercel-build.sh
+++ apps/web/vercel-build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "Installing Next.js in web app..."
+cd ../../apps/web
+npm install next react react-dom
+
 echo "Building shared package..."
 cd ../packages/shared
 npm install
@@ -13,5 +17,4 @@ npm run build
 
 echo "Building web app..."
 cd ../../apps/web
-npm install
 next build
