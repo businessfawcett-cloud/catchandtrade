3a945ef5d655d948096524d48b720ad6407bef05
businessfawcett-cloud <fawcettbusiness@gmail.com>
2026-03-22 16:37:18 -0600

    Clean up test routes

--- apps/web/src/app/api/sets/[code]/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function GET(
-  request: NextRequest,
-  { params }: { params: Promise<{ code: string }> }
-) {
-  const { code } = await params;
-  return NextResponse.json({ test: 'works', code });
-}
