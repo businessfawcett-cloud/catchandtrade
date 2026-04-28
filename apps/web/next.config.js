58b7be697114c1266d62d3ba71a1639b1915b019
businessfawcett-cloud <fawcettbusiness@gmail.com>
2026-04-27 20:20:57 -0600

    Add explicit webpack alias for @/ to fix module resolution on Render

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: false,
   typescript: {
@@ -35,6 +37,13 @@ const nextConfig = {
         permanent: true,
       },
     ];
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
